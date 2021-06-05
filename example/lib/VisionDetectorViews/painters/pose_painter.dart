14eee3cd80fb5eb4ecbbab2b951b1138c2f6cb94
madbeginner <38140590+YounseoRyu@users.noreply.github.com>
2021-06-04 17:19:36 -0700

    Add lines connecting body and arms (#49)
    * Add paintLine function
    
    Co-authored-by: Younseo Ryu <younseoryu@Younseos-MBP.attlocal.net>

--- example/lib/VisionDetectorViews/painters/pose_painter.dart
+++ example/lib/VisionDetectorViews/painters/pose_painter.dart
@@ -16,8 +16,18 @@ class PosePainter extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     final paint = Paint()
       ..style = PaintingStyle.stroke
-      ..strokeWidth = 4
+      ..strokeWidth = 4.0
       ..color = Colors.green;
+
+    final leftPaint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 3.0
+      ..color = Colors.yellow;
+
+    final rightPaint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 3.0
+      ..color = Colors.blueAccent;
 
     poses.forEach((pose) {
       pose.landmarks.forEach((_, landmark) {
@@ -29,6 +39,40 @@ class PosePainter extends CustomPainter {
             1,
             paint);
       });
+
+      void paintLine(
+          PoseLandmarkType type1, PoseLandmarkType type2, Paint paintType) {
+        PoseLandmark joint1 = pose.landmarks[type1]!;
+        PoseLandmark joint2 = pose.landmarks[type2]!;
+        canvas.drawLine(
+            Offset(translateX(joint1.x, rotation, size, absoluteImageSize),
+                translateY(joint1.y, rotation, size, absoluteImageSize)),
+            Offset(translateX(joint2.x, rotation, size, absoluteImageSize),
+                translateY(joint2.y, rotation, size, absoluteImageSize)),
+            paintType);
+      }
+
+      //Draw arms
+      paintLine(
+          PoseLandmarkType.leftShoulder, PoseLandmarkType.leftElbow, leftPaint);
+      paintLine(
+          PoseLandmarkType.leftElbow, PoseLandmarkType.leftWrist, leftPaint);
+      paintLine(PoseLandmarkType.rightShoulder, PoseLandmarkType.rightElbow,
+          rightPaint);
+      paintLine(
+          PoseLandmarkType.rightElbow, PoseLandmarkType.rightWrist, rightPaint);
+
+      //Draw Body
+      paintLine(
+          PoseLandmarkType.leftShoulder, PoseLandmarkType.leftHip, leftPaint);
+      paintLine(PoseLandmarkType.rightShoulder, PoseLandmarkType.rightHip,
+          rightPaint);
+
+      //Draw legs
+      paintLine(
+          PoseLandmarkType.leftHip, PoseLandmarkType.leftAnkle, leftPaint);
+      paintLine(
+          PoseLandmarkType.rightHip, PoseLandmarkType.rightAnkle, rightPaint);
     });
   }
 
