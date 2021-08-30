300cfef7a2c6c594d0f6296ca27270f473e91771
Nils Seip <nils@nilstschmidt.de>
2021-08-30 17:26:58 +0200

    Import MLKitLanguageID

--- ios/Classes/GoogleMlKitPlugin.h
+++ ios/Classes/GoogleMlKitPlugin.h
@@ -1,6 +1,7 @@
 #import <Flutter/Flutter.h>
 
 #import <MLKitVision/MLKitVision.h>
+#import <MLKitLanguageID/MLKitLanguageID.h>
 
 @interface GoogleMlKitPlugin : NSObject<FlutterPlugin>
 @property(nonatomic, readwrite) NSMutableDictionary *handlers;
