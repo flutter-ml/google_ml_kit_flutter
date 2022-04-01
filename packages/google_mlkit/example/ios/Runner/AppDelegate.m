d3d466ba86969cd4760917f94e14827460b5397c
Francisco Bernal <fbernaly@gmail.com>
2022-03-31 19:06:06 -0700

    Adding Firebase dependency

--- packages/google_mlkit/example/ios/Runner/AppDelegate.m
+++ packages/google_mlkit/example/ios/Runner/AppDelegate.m
@@ -1,11 +1,13 @@
 #import "AppDelegate.h"
 #import "GeneratedPluginRegistrant.h"
+@import Firebase;
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
+  [FIRApp configure];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
