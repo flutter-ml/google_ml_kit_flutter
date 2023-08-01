4580c6ad53aa369479bdc1796e738446aa53478b
Francisco Bernal <fbernaly@users.noreply.github.com>
2023-08-01 10:31:14 -0700

    refactor: Update demo app: (#489)

--- packages/example/lib/vision_detector_views/gallery_view.dart
+++ packages/example/lib/vision_detector_views/gallery_view.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_mlkit_commons/google_mlkit_commons.dart';
 import 'package:image_picker/image_picker.dart';
+
+import 'utils.dart';
 
 class GalleryView extends StatefulWidget {
   GalleryView(
@@ -74,6 +78,13 @@ class _GalleryViewState extends State<GalleryView> {
       Padding(
         padding: EdgeInsets.symmetric(horizontal: 16),
         child: ElevatedButton(
+          onPressed: _getImageAsset,
+          child: Text('From Assets'),
+        ),
+      ),
+      Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        child: ElevatedButton(
           child: Text('From Gallery'),
           onPressed: () => _getImage(ImageSource.gallery),
         ),
@@ -101,16 +112,69 @@ class _GalleryViewState extends State<GalleryView> {
     });
     final pickedFile = await _imagePicker?.pickImage(source: source);
     if (pickedFile != null) {
-      _processPickedFile(pickedFile);
+      _processFile(pickedFile.path);
     }
-    setState(() {});
   }
 
-  Future _processPickedFile(XFile? pickedFile) async {
-    final path = pickedFile?.path;
-    if (path == null) {
-      return;
-    }
+  Future _getImageAsset() async {
+    final manifestContent = await rootBundle.loadString('AssetManifest.json');
+    final Map<String, dynamic> manifestMap = json.decode(manifestContent);
+    final assets = manifestMap.keys
+        .where((String key) => key.contains('images/'))
+        .where((String key) =>
+            key.contains('.jpg') ||
+            key.contains('.jpeg') ||
+            key.contains('.png') ||
+            key.contains('.webp'))
+        .toList();
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return Dialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0)),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Select image',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  ConstrainedBox(
+                    constraints: BoxConstraints(
+                        maxHeight: MediaQuery.of(context).size.height * 0.7),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          for (final path in assets)
+                            GestureDetector(
+                              onTap: () async {
+                                Navigator.of(context).pop();
+                                _processFile(await getAssetPath(path));
+                              },
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Image.asset(path),
+                              ),
+                            ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      child: Text('Cancel')),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
+  Future _processFile(String path) async {
     setState(() {
       _image = File(path);
     });
--- packages/example/lib/vision_detector_views/object_detector_view.dart
+++ packages/example/lib/vision_detector_views/object_detector_view.dart
@@ -1,14 +1,10 @@
-import 'dart:io' as io;
-
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:google_mlkit_object_detection/google_mlkit_object_detection.dart';
-import 'package:path/path.dart';
-import 'package:path_provider/path_provider.dart';
 
 import 'detector_view.dart';
 import 'painters/object_detector_painter.dart';
+import 'utils.dart';
 
 class ObjectDetectorView extends StatefulWidget {
   @override
@@ -23,6 +19,28 @@ class _ObjectDetectorView extends State<ObjectDetectorView> {
   CustomPaint? _customPaint;
   String? _text;
   var _cameraLensDirection = CameraLensDirection.back;
+  int _option = 0;
+  final _options = {
+    'default': '',
+    'object_custom': 'object_labeler.tflite',
+    'fruits': 'object_labeler_fruits.tflite',
+    'flowers': 'object_labeler_flowers.tflite',
+    'birds': 'lite-model_aiy_vision_classifier_birds_V1_3.tflite',
+    // https://tfhub.dev/google/lite-model/aiy/vision/classifier/birds_V1/3
+
+    'food': 'lite-model_aiy_vision_classifier_food_V1_1.tflite',
+    // https://tfhub.dev/google/lite-model/aiy/vision/classifier/food_V1/1
+
+    'plants': 'lite-model_aiy_vision_classifier_plants_V1_3.tflite',
+    // https://tfhub.dev/google/lite-model/aiy/vision/classifier/plants_V1/3
+
+    'mushrooms': 'lite-model_models_mushroom-identification_v1_1.tflite',
+    // https://tfhub.dev/bohemian-visual-recognition-alliance/lite-model/models/mushroom-identification_v1/1
+
+    'landmarks':
+        'lite-model_on_device_vision_classifier_landmarks_classifier_north_america_V1_1.tflite',
+    // https://tfhub.dev/google/lite-model/on_device_vision/classifier/landmarks_classifier_north_america_V1/1
+  };
 
   @override
   void dispose() {
@@ -33,54 +51,110 @@ class _ObjectDetectorView extends State<ObjectDetectorView> {
 
   @override
   Widget build(BuildContext context) {
-    return DetectorView(
-      title: 'Object Detector',
-      customPaint: _customPaint,
-      text: _text,
-      onImage: _processImage,
-      initialCameraLensDirection: _cameraLensDirection,
-      onCameraLensDirectionChanged: (value) => _cameraLensDirection = value,
-      onCameraFeedReady: () => _initializeDetector(_mode),
-      onDetectorViewModeChanged: _onScreenModeChanged,
+    return Scaffold(
+      body: Stack(children: [
+        DetectorView(
+          title: 'Object Detector',
+          customPaint: _customPaint,
+          text: _text,
+          onImage: _processImage,
+          initialCameraLensDirection: _cameraLensDirection,
+          onCameraLensDirectionChanged: (value) => _cameraLensDirection = value,
+          onCameraFeedReady: _initializeDetector,
+          initialDetectionMode: DetectorViewMode.values[_mode.index],
+          onDetectorViewModeChanged: _onScreenModeChanged,
+        ),
+        Positioned(
+            top: 30,
+            left: 100,
+            right: 100,
+            child: Row(
+              children: [
+                Spacer(),
+                Container(
+                    decoration: BoxDecoration(
+                      color: Colors.black54,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: _buildDropdown(),
+                    )),
+                Spacer(),
+              ],
+            )),
+      ]),
     );
   }
+
+  Widget _buildDropdown() => DropdownButton<int>(
+        value: _option,
+        icon: const Icon(Icons.arrow_downward),
+        elevation: 16,
+        style: const TextStyle(color: Colors.blue),
+        underline: Container(
+          height: 2,
+          color: Colors.blue,
+        ),
+        onChanged: (int? option) {
+          if (option != null) {
+            setState(() {
+              _option = option;
+              _initializeDetector();
+            });
+          }
+        },
+        items: List<int>.generate(_options.length, (i) => i)
+            .map<DropdownMenuItem<int>>((option) {
+          return DropdownMenuItem<int>(
+            value: option,
+            child: Text(_options.keys.toList()[option]),
+          );
+        }).toList(),
+      );
 
   void _onScreenModeChanged(DetectorViewMode mode) {
     switch (mode) {
       case DetectorViewMode.gallery:
         _mode = DetectionMode.single;
-        _initializeDetector(_mode);
+        _initializeDetector();
         return;
 
       case DetectorViewMode.liveFeed:
         _mode = DetectionMode.stream;
-        _initializeDetector(_mode);
+        _initializeDetector();
         return;
     }
   }
 
-  void _initializeDetector(DetectionMode mode) async {
+  void _initializeDetector() async {
     _objectDetector?.close();
-    print('Set detector in mode: $mode');
+    _objectDetector = null;
+    print('Set detector in mode: $_mode');
 
-    // uncomment next lines if you want to use the default model
-    // final options = ObjectDetectorOptions(
-    //     mode: mode,
-    //     classifyObjects: true,
-    //     multipleObjects: true);
-    // _objectDetector = ObjectDetector(options: options);
-
-    // uncomment next lines if you want to use a local model
-    // make sure to add tflite model to assets/ml
-    final path = 'assets/ml/object_labeler.tflite';
-    final modelPath = await _getModel(path);
-    final options = LocalObjectDetectorOptions(
-      mode: mode,
-      modelPath: modelPath,
-      classifyObjects: true,
-      multipleObjects: true,
-    );
-    _objectDetector = ObjectDetector(options: options);
+    if (_option == 0) {
+      // use the default model
+      print('use the default model');
+      final options = ObjectDetectorOptions(
+        mode: _mode,
+        classifyObjects: true,
+        multipleObjects: true,
+      );
+      _objectDetector = ObjectDetector(options: options);
+    } else if (_option > 0 && _option <= _options.length) {
+      // use a custom model
+      // make sure to add tflite model to assets/ml
+      final option = _options[_options.keys.toList()[_option]] ?? '';
+      final modelPath = await getAssetPath('assets/ml/$option');
+      print('use custom model path: $modelPath');
+      final options = LocalObjectDetectorOptions(
+        mode: _mode,
+        modelPath: modelPath,
+        classifyObjects: true,
+        multipleObjects: true,
+      );
+      _objectDetector = ObjectDetector(options: options);
+    }
 
     // uncomment next lines if you want to use a remote model
     // make sure to add model to firebase
@@ -89,7 +163,7 @@ class _ObjectDetectorView extends State<ObjectDetectorView> {
     //     await FirebaseObjectDetectorModelManager().downloadModel(modelName);
     // print('Downloaded: $response');
     // final options = FirebaseObjectDetectorOptions(
-    //   mode: mode,
+    //   mode: _mode,
     //   modelName: modelName,
     //   classifyObjects: true,
     //   multipleObjects: true,
@@ -108,7 +182,7 @@ class _ObjectDetectorView extends State<ObjectDetectorView> {
       _text = '';
     });
     final objects = await _objectDetector!.processImage(inputImage);
-    print('Objects found: ${objects.length}\n\n');
+    // print('Objects found: ${objects.length}\n\n');
     if (inputImage.metadata?.size != null &&
         inputImage.metadata?.rotation != null) {
       final painter = ObjectDetectorPainter(
@@ -132,20 +206,5 @@ class _ObjectDetectorView extends State<ObjectDetectorView> {
     if (mounted) {
       setState(() {});
     }
-  }
-
-  Future<String> _getModel(String assetPath) async {
-    if (io.Platform.isAndroid) {
-      return 'flutter_assets/$assetPath';
-    }
-    final path = '${(await getApplicationSupportDirectory()).path}/$assetPath';
-    await io.Directory(dirname(path)).create(recursive: true);
-    final file = io.File(path);
-    if (!await file.exists()) {
-      final byteData = await rootBundle.load(assetPath);
-      await file.writeAsBytes(byteData.buffer
-          .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
-    }
-    return file.path;
   }
 }
