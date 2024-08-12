b3a779d915ccc9f06771df4df0e365f2415fe3c8
Benson Arafat <benpaul320@gmail.com>
2024-08-12 17:38:42 +0100

    fix: code-analysis

--- packages/google_mlkit_subject_segmentation/ios/Classes/GoogleMlkitSubjectionSegmentationPlugin.m
+++ packages/google_mlkit_subject_segmentation/ios/Classes/GoogleMlkitSubjectionSegmentationPlugin.m
@@ -10,11 +10,7 @@
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
     result(FlutterMethodNotImplemented);
-  }
 }
 
 @end
